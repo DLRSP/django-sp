5bb5d5211d372207399ed6eb0527f752b1282e5e
KingDade <davide.larosa.88@gmail.com>
2016-01-06 14:38:14 +0000

    remove absolute as vieew

--- socialprofile/admin.py
+++ socialprofile/admin.py
@@ -3,10 +3,10 @@
 # pylint: disable=R0901,R0904
 
 from django.contrib import admin
-from . import models
+from socialprofile.models import SocialProfile
 
 class CustomUserAdmin(admin.ModelAdmin):
     """Sets up the custom user admin display"""
     list_display = ('username', 'email', 'gender', 'first_name', 'last_name', 'is_staff', 'is_active', 'manually_edited')
 
-admin.site.register(models.SocialProfile, CustomUserAdmin)
+admin.site.register(SocialProfile, CustomUserAdmin)
