265d9d020e38b2ef33ecd00fe72d920e6d3f10dc
dlrsp-dev <dlrsp.dev@gmail.com>
2022-01-10 23:57:21 +0100

    - add AutoField at app level

--- src/socialprofile/apps.py
+++ src/socialprofile/apps.py
@@ -6,5 +6,6 @@ from django.apps import AppConfig
 class SocialProfileConfig(AppConfig):
     """Default configuration for socialprofile"""
 
+    default_auto_field = 'django.db.models.AutoField'
     name = "socialprofile"
     verbose_name = "Social Profile"
