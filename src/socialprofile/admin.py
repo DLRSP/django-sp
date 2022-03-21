948202828ac8a696be7d48b73e824d380f3538bd
dlrsp-dev <dlrsp.dev@gmail.com>
2022-03-21 10:46:05 +0100

    - add action for publish/unpublish model mixin

--- src/socialprofile/admin.py
+++ src/socialprofile/admin.py
@@ -1,10 +1,12 @@
 """Django Admin Site configuration for socialprofiles"""
 from axes.admin import AccessAttemptAdmin, AccessLogAdmin
 from axes.models import AccessAttempt, AccessLog
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.auth import get_permission_codename
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext
 from django_otp.plugins.otp_static.admin import StaticDeviceAdmin
 from django_otp.plugins.otp_static.models import StaticDevice
 from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
@@ -233,6 +235,56 @@ class CustomUserAdmin(ImageCroppingMixin, BaseUserAdmin):
 
 
 class ModelAdminPublishFieldsMixin(admin.ModelAdmin):
+    """Custom model mixin for Publish Date and Publish By information"""
+
+    actions = ["make_published", "make_unpublished"]
+
+    @admin.action(
+        permissions=["publish"], description=_("Mark selected object as published")
+    )
+    def make_published(self, request, queryset):
+        """Action to update selected object as published"""
+        updated = queryset.update(published=True)
+        self.message_user(
+            request,
+            ngettext(
+                "%d object was successfully marked as published.",
+                "%d objects were successfully marked as published.",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+    def has_publish_permission(self, request):
+        """Does the user have the publish permission?"""
+        opts = self.opts
+        codename = get_permission_codename("publish", opts)
+        return request.user.has_perm(f"{opts.app_label}.{codename}")
+
+    @admin.action(
+        permissions=["unpublish"], description=_("Mark selected object as unpublished")
+    )
+    def make_unpublished(self, request, queryset):
+        """Action to update selected object as unpublished"""
+        updated = queryset.update(published=False)
+        self.message_user(
+            request,
+            ngettext(
+                "%d object was successfully marked as unpublished.",
+                "%d objects were successfully marked as unpublished.",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+    def has_unpublish_permission(self, request):
+        """Does the user have the unpublish permission?"""
+        opts = self.opts
+        codename = get_permission_codename("unpublish", opts)
+        return request.user.has_perm(f"{opts.app_label}.{codename}")
+
     def save_model(self, request, obj, form, change) -> None:
         """Update publish fields from request object before save."""
         current_now = timezone.now()
@@ -246,6 +298,8 @@ class ModelAdminPublishFieldsMixin(admin.ModelAdmin):
 
 
 class ModelAdminAuditFieldsMixin(admin.ModelAdmin):
+    """Custom model mixin for Create/Modify Date and Create/Modify By information"""
+
     def save_model(self, request, obj, form, change) -> None:
         """Update audit fields from request object before save."""
         current_now = timezone.now()
